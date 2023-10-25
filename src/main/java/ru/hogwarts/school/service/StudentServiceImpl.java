d63d4be5c8d07e859a5744e7fbaa77344ccce13a
Elena <lenulka.90@mail.ru>
2023-10-25 10:34:15 +0300

    CRUD-методы для Студента.

--- src/main/java/ru/hogwarts/school/service/StudentServiceImpl.java
+++ src/main/java/ru/hogwarts/school/service/StudentServiceImpl.java
@@ -1,12 +1,57 @@
 package ru.hogwarts.school.service;
 
 import org.springframework.stereotype.Service;
+import ru.hogwarts.school.exceptions.StudentAlreadyExistException;
+import ru.hogwarts.school.exceptions.StudentNotFoundException;
+import ru.hogwarts.school.model.Student;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @Service
 public class StudentServiceImpl implements StudentService {
 
+    private final Map<Long, Student> repository = new HashMap<>();
+    private Long countID = 0L;
 
+    public Student create(Student student) {
+        if (repository.containsValue(student)) {
+            throw new StudentAlreadyExistException("Студент уже был добавлен.");
+        }
 
+        long id = ++countID;
+        student.setId(id);
+        return repository.put(id, student);
+    }
+
+    public Student read(long id) {
+        Student student = repository.get(id);
+
+        if (student == null) {
+            throw new StudentNotFoundException("Студент не найден в хранилище.")
+
+        }
+
+        return student;
+    }
+
+    public Student update(Student student) {
+        if (!repository.containsKey(student.getId())) {
+            throw new StudentNotFoundException("Студент с таким id не найден.")
+        }
+        return repository.put(student.getId(), student);
+    }
+
+    public Student delete(long id) {
+        Student removedStudent = repository.remove(id);
+        if (removedStudent == null) {
+            throw new StudentNotFoundException("Студент не найден в хранилище.")
+
+        }
+
+        return removedStudent;
+
+    }
 
 
 }
