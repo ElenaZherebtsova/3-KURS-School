2338952f3ebc08c2697d15f21eb8778850cfd655
Elena <lenulka.90@mail.ru>
2023-11-25 11:53:38 +0300

    4.1. - кол-во всех студентов, - средний возраст, - последние 5 студентов, - пагинация к аватаркам.

--- src/main/java/ru/hogwarts/school/controller/AvatarController.java
+++ src/main/java/ru/hogwarts/school/controller/AvatarController.java
@@ -1,6 +1,7 @@
 package ru.hogwarts.school.controller;
 
 import org.apache.coyote.Response;
+import org.springframework.data.domain.Page;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
@@ -8,6 +9,7 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 import ru.hogwarts.school.model.Avatar;
 import ru.hogwarts.school.service.AvatarService;
+import ru.hogwarts.school.service.AvatarServiceImpl;
 
 import java.awt.*;
 import java.io.File;
@@ -51,7 +53,7 @@ public class AvatarController {
     }
 
     @GetMapping(value = "/{id}/avatar-from-file")
-    public ResponseEntity<byte[]> downloadAvatarFromFile(@PathVariable long id) throws IOException{
+    public ResponseEntity<byte[]> downloadAvatarFromFile(@PathVariable long id) throws IOException {
         File avatar = avatarService.readFromFile(id);
         HttpHeaders headers = new HttpHeaders();
         headers.setContentType(MediaType.parseMediaType(Files.probeContentType(avatar.toPath())));
@@ -60,5 +62,11 @@ public class AvatarController {
                 .headers(headers)
                 .body(Files.readAllBytes(avatar.toPath()));
 
+    }
+
+    @GetMapping(value = "/all")
+    public Page<Avatar> getAllAvatars(@RequestParam("page") int page,
+                                      @RequestParam("size") int maxFileSize) {
+        return avatarService.getAllAvatars(page, maxFileSize);
     }
 }
--- src/main/java/ru/hogwarts/school/service/AvatarServiceImpl.java
+++ src/main/java/ru/hogwarts/school/service/AvatarServiceImpl.java
@@ -1,6 +1,9 @@
 package ru.hogwarts.school.service;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 import ru.hogwarts.school.exceptions.AvatarNotFoundException;
@@ -79,6 +82,13 @@ public class AvatarServiceImpl implements AvatarService {
 
 
     private String getExtensions(String fileName) {
+
         return fileName.substring(fileName.lastIndexOf(".") + 1);
+    }
+
+    @Override
+    public Page<Avatar> getAllAvatars(Integer pageNo, Integer pageSize) {
+        Pageable paging = PageRequest.of(pageNo, pageSize);
+        return avatarRepository.findAll(paging);
     }
 }
